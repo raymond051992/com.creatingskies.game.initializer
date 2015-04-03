13c7394e8696d8d6a3be38b10deebaf2dd7916ab
Raymond <Raymond@Home>
2015-04-03 23:35:23 +0800

    update LoginController

--- src/main/java/com/creatingskies/game/login/LoginController.java
+++ src/main/java/com/creatingskies/game/login/LoginController.java
@@ -9,9 +9,12 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+import com.creatingskies.game.classes.UserManager;
 import com.creatingskies.game.common.AlertDialog;
 import com.creatingskies.game.common.MainLayout;
 import com.creatingskies.game.main.MainController;
+import com.creatingskies.game.model.user.User;
+import com.creatingskies.game.model.user.UserDao;
 
 public class LoginController {
 
@@ -32,9 +35,23 @@ public class LoginController {
 	@FXML
 	private void login(){
 		if(usernameField.getText().isEmpty() ||
-				passwordField.getText().isEmpty()){
+			passwordField.getText().isEmpty()){
 			new AlertDialog(AlertType.ERROR, "Ooops", "Username and/or password is required.", null).showAndWait();
 		}else{
+			UserDao userDao = new UserDao();
+			User user = userDao.findUser(usernameField.getText());
+			
+			if(user == null){
+				new AlertDialog(AlertType.ERROR, "Ooops", "Inavlid username/password.", null).showAndWait();
+				return;
+			}
+			
+			if(!user.getPassword().equals(passwordField.getText())){
+				new AlertDialog(AlertType.ERROR, "Ooops", "Inavlid username/password.", null).showAndWait();
+				return;
+			}
+			
+			UserManager.setCurrentUser(user);
 			new MainController().show();
 		}
 	}
