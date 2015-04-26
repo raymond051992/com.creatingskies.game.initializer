f28dee3ee990c50ae9a0f070d0cf2f12264b1b8e
jahnavarro <ganjaboi.navarro@gmail.com>
2015-04-27 07:06:31 +0800

    added statistics

--- src/main/java/com/creatingskies/game/statistics/StatisticsController.java
+++ src/main/java/com/creatingskies/game/statistics/StatisticsController.java
@@ -1,30 +1,46 @@
 package com.creatingskies.game.statistics;
 
 import java.io.IOException;
-import java.text.DateFormatSymbols;
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.Random;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
 
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.StringConverter;
+
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
 
 import com.creatingskies.game.classes.PropertiesViewController;
 import com.creatingskies.game.common.MainLayout;
+import com.creatingskies.game.core.Game;
+import com.creatingskies.game.core.GameDao;
+import com.creatingskies.game.core.GameResult;
+import com.creatingskies.game.model.company.Company;
+import com.creatingskies.game.model.company.CompanyDAO;
+import com.creatingskies.game.util.Util;
 
-public class StatisticsController extends PropertiesViewController{
+public class StatisticsController extends PropertiesViewController {
 
-	@FXML private BarChart<String, Integer> barChart;
+	@FXML private BarChart<String, Double> barChart;
     @FXML private CategoryAxis xAxis;
 
-    private ObservableList<String> monthNames = FXCollections.observableArrayList();
-    private boolean setAlready;
+    @FXML private DatePicker startDatePicker;
+    @FXML private DatePicker endDatePicker;
+    
+    @FXML private ChoiceBox<Game> gameChoices;
+    @FXML private ChoiceBox<Company> companyChoices;
+    
+    private GameDao gameDao;
     
     public void show(){
 		try {
@@ -39,37 +55,91 @@ public class StatisticsController extends PropertiesViewController{
 
     @FXML
     private void initialize() {
-    	String[] months = DateFormatSymbols.getInstance(Locale.ENGLISH).getMonths();
-        monthNames.addAll(Arrays.asList(months));
-        xAxis.setCategories(monthNames);
+    	super.init();
+    	gameDao = new GameDao();
+    	
+    	gameChoices.getItems().add(null);
+		gameChoices.getItems()
+				.addAll(FXCollections.observableArrayList(new GameDao()
+						.findAllGames()));
+		gameChoices.setConverter(new StringConverter<Game>() {
+			@Override
+			public String toString(Game game) {
+				return game != null ? game.getTitle() : null;
+			}
+			
+			@Override
+			public Game fromString(String string) {
+				return null;
+			}
+		});
+		
+		companyChoices.getItems().add(null);
+		companyChoices.getItems().addAll(
+				FXCollections.observableArrayList(new CompanyDAO()
+						.findAllCompanies()));
+		companyChoices.setConverter(new StringConverter<Company>() {
+			@Override
+			public String toString(Company company) {
+				return company != null ? company.getName() : null;
+			}
+
+			@Override
+			public Company fromString(String string) {
+				return null;
+			}
+		});
+		
+		gameChoices.getSelectionModel().selectFirst();
+		companyChoices.getSelectionModel().selectFirst();
     }
 
     @FXML
-    public void generate() {
-    	if(!setAlready){
-    		setDummyData();
-        	setDummyData();
-        	setDummyData();
-        	setAlready = true;
-    	}
-    }
-    
-    public void setDummyData(){
-    	int[] monthCounter = new int[12];
+    public void filterResults() {
+    	barChart.getData().clear();
+    	
+    	Criterion[] criterions = new Criterion[4];
+		
+		if(startDatePicker.getValue() != null){
+			criterions[0] = Restrictions.ge("entryDate",
+					Date.from(Instant.from(startDatePicker.getValue().atStartOfDay(ZoneId.systemDefault()))));
+		}
+		
+		if(endDatePicker.getValue() != null){
+			criterions[1] = Restrictions.le("entryDate",
+					Util.addDays(Util.toDate(endDatePicker.getValue()), 1));
+		}
+		
+		if(gameChoices.getValue() != null){
+			criterions[2] = Restrictions.eq("game", gameChoices.getValue());
+		}
+		
+		if(companyChoices.getValue() != null){
+			criterions[3] = Restrictions.eq("company.name", companyChoices.getValue().getName());
+		}
+    	
+    	List<GameResult> results = gameDao.findAllGameResults(criterions);
+    	
+    	XYChart.Series<String, Double> series = null;
+        GameResult previousResult = null;
+        boolean newGroup = false;
         
-        for (int i = 0; i < 100; i++) {
-        	Random random = new Random();
-            int month = random.nextInt(12);
-            monthCounter[month]++;
+        for (GameResult result : results) {
+        	if(previousResult == null || !previousResult.equals(result)){
+        		series = new XYChart.Series<>();
+        		newGroup = true;
+        	} else {
+        		newGroup = false;
+        	}
+        	
+			series.getData().add(
+					new XYChart.Data<>(result.getGroup().getName(), result
+							.getDuration()));
+			
+			if(newGroup){
+				barChart.getData().add(series);
+			}
         }
-
-        XYChart.Series<String, Integer> series = new XYChart.Series<>();
-
-        for (int i = 0; i < monthCounter.length; i++) {
-            series.getData().add(new XYChart.Data<>(monthNames.get(i), monthCounter[i]));
-        }
-
-        barChart.getData().add(series);
     }
     
 	@Override
